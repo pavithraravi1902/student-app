07391610e9bdbdd9849dfd5b14cc4dfc8f989bac
pavithra <pavithraravi1902@gmail.com>
2022-02-23 10:23:59 +0530

    changes updated

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -25,16 +25,48 @@ export class DataService {
     { subName: "Differential Equation", subCode: "TAM3A" },
     { subName: "Financial Accounting", subCode: "SPAM1" },
     { subName: "Cost Accounting", subCode: "SPAM2" }];
-  public Algebra: any = [
+  public algebra: any = [
     { unit1: "Abelian, non-abelian group" },
     { unit2: "Rings" },
     { unit3: "Cyclic, non-cyclic subgroup" },
-  ]
+  ];
+  public trignometry: any = [
+    { unit1: "Expansion of sin theta, cos theta" },
+    { unit2: "power of cos theta, sin theta" },
+    { unit3: "Hyperbolic Function" },
+  ];
+  public differentialEquation: any = [
+    { unit1: "Characteristics Equation" },
+    { unit2: "Laplase Function" },
+    { unit3: "Fourier series" },
+  ];
+  public financialAccounting: any = [
+    { unit1: "Final Accounts" },
+    { unit2: "Depreciation" },
+    { unit3: "Partnership" },
+  ];
+  public costAccounting: any = [
+    { unit1: "Fundamental of cost accounting" },
+    { unit2: "Methods and techniques of cost accounting" },
+    { unit3: "Accounting for overheads" },
+  ];
   constructor() { }
   getData(): any {
     return this.subjectData;
   }
-  getAlgebra(){
-    return this.Algebra;
+  getAlgebra() {
+    return this.algebra;
+  }
+  getTrigno() {
+    return this.trignometry;
+  }
+  getDiff() {
+    return this.differentialEquation;
+  }
+  getFinancial() {
+    return this.financialAccounting;
+  }
+  getCost() {
+    return this.costAccounting;
   }
 }
--- src/app/syllabus/syllabus.component.ts
+++ src/app/syllabus/syllabus.component.ts
@@ -8,15 +8,49 @@ import { DataService } from '../data.service';
   <thead>
     <th>Subject Name</th>
     <th>Subject Code</th>
-    </thead>
-    <tbody>
-    <td>
+    <th></th>
+  </thead>
+  <tbody>
+    <tr>
+       <td>
     <div *ngFor="let elt of subjectData">
-    <td colspan="2"><a>{{elt.subName}}</a></td>
+      <td> {{elt.subName}} </td>
       <td>{{elt.subCode}} </td>
-      <td><button (click)="onclick()">syllabus</button>
-      </div>
-    </td>
+    </div>
+      <td>
+       <tr>
+          <button (click)="algebraFn()">Syllabys</button>
+          <ul *ngFor="let item of data; index as i">
+          <li> {{"unit-" + (i+1)}}  {{(item)}}</li>
+          </ul>
+       </tr>
+       <tr>
+          <button (click)="trignoFn()">Syllabys</button>
+          <ul *ngFor="let item1 of data1; index as i">
+          <li> {{"unit-" + (i+1)}}  {{(item1)}}</li>
+          </ul>
+       </tr>
+       <tr>
+          <button (click)="diffEqfn()">Syllabys</button>
+          <ul *ngFor="let item2 of data2; index as i">
+          <li> {{"unit-" + (i+1)}}  {{(item2)}}</li>
+          </ul>
+       </tr>
+        <tr>
+          <button (click)="financialAccFn()">Syllabys</button>
+          <ul *ngFor="let item3 of data3; index as i">
+          <li> {{"unit-" + (i+1)}}  {{(item3)}}</li>
+          </ul>
+        </tr>
+        <tr>
+          <button (click)="costFn()">Syllabys</button>
+          <ul *ngFor="let item4 of data4; index as i">
+          <li> {{"unit-" + (i+1)}}  {{(item4)}}</li>
+          </ul>
+        </tr>
+       </td>
+       </tr>
+
     </tbody>
     </table>
   `,
@@ -26,15 +60,46 @@ import { DataService } from '../data.service';
 export class SyllabusComponent implements OnInit {
   public subjectData: any = [];
   public algebra: any = [];
-  public data: string="";
+  public trigno: any = [];
+  public diffEq: any = [];
+  public financialAcc: any = [];
+  public costAcc: any = [];
+  public data: Array<string> = [];
+  public data1: Array<string> = [];
+  public data2: Array<string> = [];
+  public data3: Array<string> = [];
+  public data4: Array<string> = [];
   constructor(private dataServie: DataService) { }
   ngOnInit(): void {
     this.subjectData = this.dataServie.getData()
     this.algebra = this.dataServie.getAlgebra();
+    this.trigno = this.dataServie.getTrigno();
+    this.diffEq = this.dataServie.getDiff();
+    this.financialAcc = this.dataServie.getFinancial();
+    this.costAcc = this.dataServie.getCost();
   }
-  onclick(){
-    this.data=this.algebra[this.data.length].unit1;
-    console.log(this.data);
-    }
+  algebraFn() {
+    "Algebra syllabus:"
+    this.data = this.algebra.filter((item: any) => Object.values(item));
+    //this.data = Object.values(this.algebra)
+    console.log(this.data)
+    return this.data;
   }
+
+  trignoFn() {
+    this.data1=this.trigno.filter((item:any)=>item);
+  }
+
+  diffEqfn() {
+    this.data2
+  }
+
+  financialAccFn() {
+    this.data3
+  }
+
+  costFn() {
+    this.data4
+  }
+}
 
